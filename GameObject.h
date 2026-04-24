40b147486911436f3b5a638786908ea98d125d84
Abdullah <itsiabdullah@gmail.com>
2026-04-24 11:18:13 +0500

    class GameObject modified

---
+++ GameObject.h
@@ -0,0 +1,27 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include "raylib.h"
+
+// Base class - the starting point for everything in our game
+// Using virtual functions here because we need polymorphism for our project
+class GameObject {
+protected:
+    float x, y;
+    bool active;
+public:
+    GameObject(float startX, float startY) : x(startX), y(startY), active(true) {}
+    virtual ~GameObject() {} 
+
+    // Pure virtual - every child class must have these or the code won't compile
+    virtual void Update() = 0; 
+    virtual void Draw(bool useTextures) const = 0; 
+
+    // Standard getters/setters for encapsulation
+    void setInactive() { active = false; }
+    bool isActive() const { return active; }
+    float getX() const { return x; }
+    float getY() const { return y; }
+};
+
+#endif
